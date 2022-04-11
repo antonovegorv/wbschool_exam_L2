f8561feafc23c590ace6309e12e1369466dc9307
Egor Antonov <antonovegorv@gmail.com>
2022-04-11 18:38:47 +0300

    complete dev04 task

--- develop/dev04/task.go
+++ develop/dev04/task.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
 /*
 === Поиск анаграмм по словарю ===
 
@@ -20,5 +26,50 @@ package main
 */
 
 func main() {
+	w := []string{"австралопитек", "ватерполистка", "обезьянство", "светобоязнь", "импортер", "пирометр", "реимпорт"}
+	a := findAnagrams(w)
+	fmt.Println(a)
+}
 
+// findAnagrams — makes a map with anagrams.
+func findAnagrams(w []string) map[string][]string {
+	t := make(map[string][]string) // Initialize temporary map.
+	r := make(map[string][]string) // Initialize result map.
+
+	// We go through each word and sort its letters. Due to this, we organize a map from anagrams.
+	for _, v := range w {
+		word := strings.ToLower(v)
+		wordSlice := strings.Split(word, "")
+		sort.Strings(wordSlice)
+		t[strings.Join(wordSlice, "")] = append(t[strings.Join(wordSlice, "")], word)
+	}
+
+	// Exclude duplicates from temporary map.
+	for k := range t {
+		set := set(t[k])
+
+		if len(set) > 1 {
+			r[t[k][0]] = set
+		}
+	}
+
+	return r
+}
+
+// set — makes set from a slice.
+func set(slice []string) []string {
+	u := make(map[string]bool)
+
+	for _, v := range slice {
+		u[v] = true
+	}
+
+	keys := make([]string, 0, len(u))
+	for k := range u {
+		keys = append(keys, k)
+	}
+
+	sort.Strings(keys)
+
+	return keys
 }
--- develop/dev04/task_test.go
+++ develop/dev04/task_test.go
@@ -1,1 +1,52 @@
 package main
+
+import "testing"
+
+func Equal(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
+type findAnagramsTest struct {
+	data     []string
+	expected map[string][]string
+}
+
+var findAnagramsTests = []findAnagramsTest{
+	{
+		[]string{"Пятак", "пятка", "Тяпка", "листок", "слиток", "столиК", "привет"},
+		map[string][]string{
+			"пятак":  {"пятак", "пятка", "тяпка"},
+			"листок": {"листок", "слиток", "столик"},
+		},
+	},
+	{
+		[]string{"австралопитек", "ватерполистка", "обезьянство", "светобоязнь", "импортер", "пирометр", "реимпорт"},
+		map[string][]string{
+			"австралопитек": {"австралопитек", "ватерполистка"},
+			"импортер":      {"импортер", "пирометр", "реимпорт"},
+			"обезьянство":   {"обезьянство", "светобоязнь"},
+		},
+	},
+}
+
+func TestGetAnagrams(t *testing.T) {
+	for _, test := range findAnagramsTests {
+		result := findAnagrams(test.data)
+		if len(result) != len(test.expected) {
+			t.Errorf("Incorrect result. Expect: %v, Got %v\n", test.expected, result)
+		}
+		for k, v := range result {
+			if !Equal(v, test.expected[k]) {
+				t.Errorf("Incorrect result. Expect: %v, Got %v\n", test.expected[k], v)
+			}
+		}
+	}
+}
