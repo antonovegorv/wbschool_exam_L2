bf2fd202c1e59bab0a80a42ba22802ec931d44b6
Egor Antonov <antonovegorv@gmail.com>
2022-04-12 20:20:45 +0300

    complete dev03 task

--- develop/dev03/task.go
+++ develop/dev03/task.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"flag"
+	"log"
+
+	"github.com/antonovegorv/wbschool_exam_L2/develop/dev03/sorter"
+)
+
 /*
 === Утилита sort ===
 
@@ -26,5 +33,29 @@ package main
 */
 
 func main() {
+	k := flag.Int("k", 0, "column to sort")
+	n := flag.Bool("n", false, "compare according to string numerical value")
+	r := flag.Bool("r", false, "reverse the result of comparisons")
+	u := flag.Bool("u", false, "output unique lines only")
+	m := flag.Bool("M", false, "sort by monthes")
+	b := flag.Bool("b", false, "trim right spaces")
+	c := flag.Bool("c", false, "check for sorted input; do not sort")
+	h := flag.Bool("h", false, "sort by numeric with suffix")
+	flag.Parse()
 
+	options := make(map[string]bool)
+	options["n"] = *n
+	options["r"] = *r
+	options["u"] = *u
+	options["M"] = *m
+	options["b"] = *b
+	options["c"] = *c
+	options["h"] = *h
+
+	if filename := flag.Arg(0); filename != "" {
+		s := sorter.New(filename, options, *k)
+		s.Run()
+	} else {
+		log.Fatalln("Please, provide a file to sort!")
+	}
 }
